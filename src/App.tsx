5cec611e6d66beb40316dabb711c58be83bba798
Aern-do <radeonchik.new@gmail.com>
2024-10-03 10:09:10 +0300

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0);
+interface Task {
+  content: string
+}
 
-  return <div className="flex items-center justify-center w-screen h-screen">
-    <p>{count}</p>
-    <button onClick={() => setCount(count + 1)}>+</button>
+function App() {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+
+  const appendTask = (task: string) => {
+    setTasks([{ content: task }, ...tasks])
+  }
+
+  return <div className="gap-2 p-2">
+    <div className="flex gap-2">
+      <input className="border-2 border-black rounded p-1" placeholder="Task description" onChange={(e) => setInput(e.target.value)} />
+      <button className="border-2 border-black rounded  p-1" onClick={() => appendTask(input)}>Add task</button>
+    </div>
+
+    <div className="flex flex-col gap-2">
+      {tasks.map((task, i) => <>
+        <p>{task.content}</p>
+        {i != tasks.length - 1 ? <div className="bg-gray-300 h-0.5 rounded" /> : null}
+      </>)}
+    </div>
   </div>
 
 }
